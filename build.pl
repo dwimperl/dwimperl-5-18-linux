4c68ca64db678e9ed52fbcc8b21146c9cc0c1e32
Gabor Szabo <gabor@szabgab.com>
2013-12-01 22:39:35 +0200

    quitly

--- build.pl
+++ build.pl
@@ -2,14 +2,14 @@
 use strict;
 use warnings;
 
+my $options = "-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no";
+my $remote = 'root@s13';
 
 my $start_time = time;
-use Net::SSH qw(ssh);
-system 'scp bootstrap.sh root@s13:';
-system 'scp README.txt root@s13:';
-system 'scp empty.pod root@s13:';
-system 'scp -r t/ root@s13:';
-ssh('root@s13', "./bootstrap.sh");
-
+foreach my $src ('bootstrap.sh', 'README.txt', 'empty.pod', 't/' ) {
+	system "scp -q $options -r $src $remote:";
+}
+system "ssh -q $options $remote ./bootstrap.sh";
 my $end_time = time;
 printf "Elapsed time: %s\n", ($end_time - $start_time)
+
