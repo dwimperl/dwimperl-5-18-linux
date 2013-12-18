fca12631ffed73fb7141ad56d63b71d63b8b0b89
Gabor Szabo <gabor@szabgab.com>
2013-12-18 08:17:20 +0200

    try to add some more XML-related modules

--- bootstrap.sh
+++ bootstrap.sh
@@ -248,6 +248,39 @@ cpanm YAML
 # LIBRARY_PATH
 cpanm XML::LibXML --configure-args "LIBS='-L$PREFIX_C/lib/' INC='-I$PREFIX_C/include/ -I/$PREFIX_C/include/libxml2'"
 
+
+# XML::Parser need expat http://sourceforge.net/projects/expat/
+EXPAT=expat-2.1.0
+wget http://downloads.sourceforge.net/project/expat/expat/2.1.0/$EXPAT.tar.gz
+tar xzf $EXPAT.tar.gz
+cd $EXPAT
+./configure --prefix $PREFIX_C
+make
+make install
+# If you ever happen to want to link against installed libraries
+# in a given directory, LIBDIR, you must either use libtool, and
+# specify the full pathname of the library, or use the `-LLIBDIR'
+# flag during linking and do at least one of the following:
+#    - add LIBDIR to the `LD_LIBRARY_PATH' environment variable
+#      during execution
+#    - add LIBDIR to the `LD_RUN_PATH' environment variable
+#      during linking
+#    - use the `-Wl,-rpath -Wl,LIBDIR' linker flag
+#    - have your system administrator add LIBDIR to `/etc/ld.so.conf'
+# 
+# See any operating system documentation about shared libraries for
+# more information, such as the ld(1) and ld.so(8) manual pages.
+
+cpanm XML::Parser --configre-args = "EXPATLIBPATH=$PREFIX_C/lib EXPATINCPATH=$PREFIX_C/include"
+
+cpanm XML::SAX::Writer
+cpanm XML::Simple
+cpanm XML::XPath
+
+
+# Finished installing Perl modules, let's test now and create the tarball
+
+
 cp $BUILD_HOME/README.txt $ROOT
 if [ ! -d $ROOT/t ]; then
     cp -r $BUILD_HOME/t $ROOT
